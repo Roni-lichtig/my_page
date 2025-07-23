6593c5be578d991048aacde6b90351a2c06e51fc
Roni-lichtig <ronili@mta.ac.il>
2025-07-23 22:48:38 +0300

    Update res_name.js

--- res_name.js
+++ res_name.js
@@ -4,4 +4,9 @@ const restaurantName = urlParams.get('restaurant');
 if (restaurantName) {
   const decodedName = decodeURIComponent(restaurantName);
   document.getElementById('resTitle').textContent = decodedName;
+
+  const bookBtn = document.getElementById('bookBtn');
+  if (bookBtn) {
+    bookBtn.href = `reserve_form.html?restaurant=${encodeURIComponent(decodedName)}`;
+  }
 }
